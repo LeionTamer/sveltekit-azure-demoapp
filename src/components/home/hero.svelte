3eab858d39cdd6af5819669e94c8e08972ba55d2
leon carbonell <leoncarbonell@gmail.com>
2023-01-29 08:33:41 +1030

    Moved to previous commit

--- src/components/home/hero.svelte
+++ src/components/home/hero.svelte
@@ -1,3 +1,15 @@
-<section id="hero" class='container p-6'>
-  We can be heros
+<section id="hero">
+	<div class="hero min-h-[70vh] bg-base-200">
+		<div class="hero-content  flex-col lg:flex-row-reverse">
+			<img
+				src="https://placeimg.com/260/400/arch"
+				class="max-w-sm rounded-lg shadow-2xl"
+				alt="my logo"
+			/>
+			<div>
+				<h1 class="text-5xl font-bold">Leon Carbonell</h1>
+				<p class="py-6">Software Engineer</p>
+			</div>
+		</div>
+	</div>
 </section>
---
+++ src/components/home/home-food.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	// import brekkie from './food/brekkie.jpg';
+	// import burger from './food/burger.jpg';
+	// import falafel from './food/falafel.jpg';
+	// import quiche from './food/quiche.jpg';
+	// import sandwich from './food/sandwich.jpg';
+</script>
+
+<section id="food">
+	<div class="hero min-h-[30vh] bg-base-300">
+		<div class="hero-content flex-col lg:flex-row">
+			<div class="carousel w-full md:w-[50%]">
+				<!-- <img src={brekkie} alt="brekkie" class="object-fit-cover w-[300px]" />
+				<img src={burger} alt="brekkie" class="object-fit-cover w-[300px]" />
+				<img src={falafel} alt="brekkie" class="object-fit-cover w-[300px]" />
+				<img src={quiche} alt="brekkie" class="object-fit-cover w-[300px]" />
+				<img src={sandwich} alt="brekkie" class="object-fit-cover w-[300px]" /> -->
+				Image goes here
+			</div>
+			<div>
+				<h1 class="text-5xl font-bold">Foodie</h1>
+				<p class="py-6">Food Aventures</p>
+			</div>
+		</div>
+	</div>
+</section>
